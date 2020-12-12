529d0ac08249cb00f2c917e92d2b0af5be1a0bf6
Alisson Gustavo <gustavoalisson112@gmail.com>
2020-12-12 01:47:43 -0300

    primeiro hello world c#

---
+++ primeiroConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace primeiroConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo! Iniciando estudos no C#.");
+        }
+    }
+}
