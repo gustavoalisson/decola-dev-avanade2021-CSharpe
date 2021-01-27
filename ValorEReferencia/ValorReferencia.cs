f4266ac94feb7ff584ea65b5d48f50ace7ce8749
Alisson Gustavo <gustavoalisson112@gmail.com>
2021-01-27 01:03:58 -0300

    assunto de fundamentos

---
+++ ValorEReferencia/ValorReferencia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprendendoCSharpe.ValorEReferencia
+{
+    class ValorReferencia
+    {
+        //Passando um valor por referência
+        static void Main()
+        {
+            int valor = 15;
+            DobraValor(ref valor);
+            Console.WriteLine(valor);
+
+        }
+
+        static void DobraValor(ref int x)
+        {
+            x *= 2;
+        }
+    }
+
+}
