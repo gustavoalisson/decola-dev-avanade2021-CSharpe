b975ec376d0b3b1bd29089f00d20a2decb97afb5
Alisson Gustavo <gustavoalisson112@gmail.com>
2021-01-27 01:05:04 -0300

    primeito commit de assuntos de POO

---
+++ ASSUNTOS-POO/Conceito-Inicial/Conta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace AprendendoCSharpe.ASSUNTOS_POO.Conceito_Inicial
+{
+    // modificador public: classe está visível a outras classes
+    public class Conta
+    {
+        public double Saldo { get; set; }
+        public double Limite { get; set; }
+        public int Numero { get; set; }
+
+    }
+}
---
+++ ASSUNTOS-POO/Conceito-Inicial/Programa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace AprendendoCSharpe.ASSUNTOS_POO.Conceito_Inicial
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            Console.WriteLine("Por favor, digite o seu nome:");
+            nome = Console.ReadLine();
+
+
+            // criando a instância da Conta
+            // É possível criar várias instâncias dessa classe
+            Conta contaA = new Conta();
+
+            contaA.Saldo = 5000;
+            contaA.Limite = 1000;
+            contaA.Numero = 1010;
+
+            Console.WriteLine($"Olá {nome}, o seu saldo é de {contaA.Saldo}");
+            Console.WriteLine($"O seu limite para saque é de {contaA.Limite}");
+            Console.WriteLine($"O número da sua conta é {contaA.Numero}");
+
+
+        }
+    }
+}
